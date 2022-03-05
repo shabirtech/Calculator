2a20c6450f126cec664c07257c554c10a9ecb98a
Shabbir <52547561+shabirtech@users.noreply.github.com>
2022-03-05 23:55:05 +0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,63 @@
+from tkinter import Tk,ttk
+import tkinter as tk
+
+LARGE_FONT_STYLE=("Arial",40, "bold")
+SMALL_FONT_STYLE=("Arial",16)
+LIGHT_GRAY="#F5F5F5"
+WHITE="#FFFFFF"
+LABEL_COLOR="#25265E"
+class Calculator:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.geometry("375x667")
+        self.window.resizable(0,0)
+        self.window.title("Calculator")
+
+        self.total_expression="0"
+        self.current_expression="0"
+        self.disply_frame=self.create_display_frame()
+        self.total_label,self.label=self.create_display_labels()
+        self.digits={
+            7:(1,1), 8:(1,2), 9:(1,3),
+            4:(2,1), 5:(2,2), 6:(2,3),
+            1:(3,1), 2:(3,2), 3:(3,3),
+            0:(4,2),".":(4,1),
+        }
+        self.button_frame=self.create_button_frame()
+        self.create_digit_buttons()
+
+    def create_display_labels(self):
+        total_label=tk.Label(self.disply_frame, text=self.total_expression, anchor=tk.E, bg=LIGHT_GRAY, fg=LABEL_COLOR, padx=24, font=SMALL_FONT_STYLE)
+        total_label.pack(expand=True,fill="both")
+
+
+        label=tk.Label(self.disply_frame, text=self.current_expression, anchor=tk.E, bg=LIGHT_GRAY, fg=LABEL_COLOR, padx=24, font=LARGE_FONT_STYLE)
+        label.pack(expand=True,fill="both")
+
+        return total_label,label
+
+    
+
+    def create_display_frame(self):
+        frame=tk.Frame(self.window, height=121, bg=LIGHT_GRAY)
+        frame.pack(expand=True,fill="both")
+        return frame
+
+    def create_digit_buttons(self):
+        for digit, grid_value in self.digits.items():
+            button=tk.Button(self.button_frame,text=str(digit), bg=WHITE,fg=LABEL_COLOR)
+            button.grid(row=grid_value[0], column=grid_value[1], sticky=tk.NSEW)
+
+    def create_button_frame(self):
+        frame=tk.Frame(self.window)
+        frame.pack(expand=True,fill="both")
+        return frame
+
+    def run(self):
+        self.window.mainloop()
+    
+if __name__ =="__main__":
+    calc=Calculator()
+    calc.run()
+
+
